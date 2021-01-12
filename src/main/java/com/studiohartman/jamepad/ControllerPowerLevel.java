04c10dce0e349224d952cc962f848f1873c7a9a2
Benjamin Schulte <bs@golfgl.de>
2021-01-12 20:48:28 +0100

    Remove unnecessary SDL prefix from power level enum constants

--- src/main/java/com/studiohartman/jamepad/ControllerPowerLevel.java
+++ src/main/java/com/studiohartman/jamepad/ControllerPowerLevel.java
@@ -4,9 +4,6 @@ package com.studiohartman.jamepad;
  * This is an enumerated type for power level of controllers.
  * <p>
  * Derived from enum SDL_JoystickPowerLevel in SDL_joystick.h.
- * <p>
- * We start with an index -1, so different from most other enums, we need a helper method to
- * convert the SDL-returned int to the enum
  *
  * @author Benjamin Schulte
  */
@@ -14,30 +11,31 @@ public enum ControllerPowerLevel {
     /**
      * Power level unknown
      */
-    SDL_JOYSTICK_POWER_UNKNOWN,
+    POWER_UNKNOWN,
     /**
      * Power level 0-5%
      */
-    SDL_JOYSTICK_POWER_EMPTY,   /* <= 5% */
+    POWER_EMPTY,   /* <= 5% */
     /**
      * Power level 6-20%
      */
-    SDL_JOYSTICK_POWER_LOW,     /* <= 20% */
+    POWER_LOW,     /* <= 20% */
     /**
      * Power level 21-70%
      */
-    SDL_JOYSTICK_POWER_MEDIUM,  /* <= 70% */
+    POWER_MEDIUM,  /* <= 70% */
     /**
      * Power level 71-100%
      */
-    SDL_JOYSTICK_POWER_FULL,    /* <= 100% */
+    POWER_FULL,    /* <= 100% */
     /**
      * Controller is wired
      */
-    SDL_JOYSTICK_POWER_WIRED,
-    SDL_JOYSTICK_POWER_MAX;
+    POWER_WIRED,
+    POWER_MAX;
 
     public static ControllerPowerLevel valueOf(int n) {
+        // C enum starts with -1, so we need to increment by one
         return ControllerPowerLevel.values()[n + 1];
     }
 
